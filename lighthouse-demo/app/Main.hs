14f43141951c51654e7e3bf1d45a2d2dc2457f05
fwcd <fwcdmail@gmail.com>
2020-02-14 19:49:41 +0100

    Implement image loading using monad transformers

--- lighthouse-demo/app/Main.hs
+++ lighthouse-demo/app/Main.hs
@@ -3,8 +3,9 @@ module Main where
 
 import qualified Codec.Picture as P
 import Control.Monad (void)
+import Control.Monad.Except (liftEither)
 import Control.Monad.Trans (lift, liftIO)
-import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Except
 import qualified Data.Text as T
 import Lighthouse.Authentication
 import Lighthouse.Connection
@@ -17,19 +18,22 @@ import System.Random
 -- | Renders a single image to the lighthouse.
 app :: String -> LighthouseIO ()
 app imagePath = do
-    optDimg <- liftIO $ P.readPng imagePath
-    case optDimg of
+    res <- runExceptT $ do
+        dimg <- liftEither =<< (liftIO $ P.readPng imagePath)
+        d <- liftEither $ dynImgToDisplay dimg
+        lift $ sendDisplay d
+        
+    case res of
         Left e -> liftIO $ putStrLn e
-        Right dimg -> void $ runMaybeT $ do
-            d <- liftMaybe $ dynImgToDisplay dimg
-            lift $ sendDisplay d
+        _ -> return ()
+    
     sendClose
 
-dynImgToDisplay :: P.DynamicImage -> Maybe Display
+dynImgToDisplay :: P.DynamicImage -> Either String Display
 dynImgToDisplay dimg = case dimg of
-    P.ImageRGB8 img -> Just $ imgToDisplay img $ \(P.PixelRGB8 r g b) -> Color (fromIntegral r) (fromIntegral g) (fromIntegral b)
-    P.ImageRGBA8 img -> Just $ imgToDisplay img $ \(P.PixelRGBA8 r g b _) -> Color (fromIntegral r) (fromIntegral g) (fromIntegral b)
-    _ -> Nothing
+    P.ImageRGB8 img -> Right $ imgToDisplay img $ \(P.PixelRGB8 r g b) -> Color (fromIntegral r) (fromIntegral g) (fromIntegral b)
+    P.ImageRGBA8 img -> Right $ imgToDisplay img $ \(P.PixelRGBA8 r g b _) -> Color (fromIntegral r) (fromIntegral g) (fromIntegral b)
+    _ -> Left "Unrecognized PNG format"
 
 imgToDisplay :: P.Pixel a => P.Image a -> (a -> Color) -> Display
 imgToDisplay img pxToColor = Display $ (\y -> Row $ (\x -> pxToColor $ P.pixelAt img x y) <$> [0..width - 1]) <$> [0..height - 1]
