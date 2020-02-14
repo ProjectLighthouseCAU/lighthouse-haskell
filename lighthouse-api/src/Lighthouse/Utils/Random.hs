c52361a90eeceb4110cdaf1275f6700f0ce5f420
fwcd <fwcdmail@gmail.com>
2020-02-14 18:27:59 +0100

    Add convenience functions for random monads

--- lighthouse-api/src/Lighthouse/Utils/Random.hs
+++ lighthouse-api/src/Lighthouse/Utils/Random.hs
@@ -1,6 +1,40 @@
-module Lighthouse.Utils.Random (nRandomsR, nRandoms) where
+module Lighthouse.Utils.Random (RandomM (..), runRandomMIO, getRandomGen, randomM, randomRM, nRandomsR, nRandoms) where
 
 import System.Random
+
+data RandomM g a = RandomM { runRandomM :: g -> (a, g) }
+
+instance Functor (RandomM g) where
+    fmap f r = RandomM $ \g -> let (x, g') = runRandomM r g
+                               in (f x, g')
+
+instance Applicative (RandomM g) where
+    pure x = RandomM $ \g -> (x, g)
+    rf <*> r = RandomM $ \g -> let (x, g') = runRandomM r g
+                                   (f, g'') = runRandomM rf g'
+                               in (f x, g'')
+
+instance Monad (RandomM g) where
+    r >>= f = RandomM $ \g -> let (x, g') = runRandomM r g
+                              in runRandomM (f x) g'
+
+-- | Runs a random monad using the global RNG.
+runRandomMIO :: RandomM StdGen a -> IO a
+runRandomMIO r = do
+    gen <- getStdGen
+    return $ fst $ runRandomM r gen
+
+-- | Fetches the generator inside the random monad.
+getRandomGen :: RandomM g g
+getRandomGen = RandomM $ \g -> (g, g)
+
+-- | Generates a random value inside the random monad.
+randomM :: (RandomGen g, Random a) => RandomM g a
+randomM = RandomM random
+
+-- | Generates a bounded random value inside the random monad.
+randomRM :: (RandomGen g, Random a) => (a, a) -> RandomM g a
+randomRM r = RandomM $ randomR r
 
 -- | Generates n random values in the given range without consuming the generator.
 nRandomsR :: (RandomGen g, Random a) => Int -> ([a], [a]) -> g -> ([a], g)
